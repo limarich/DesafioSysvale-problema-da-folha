b55c970cd5214224746080ff44f0e5741fda25eb
Richard <richard.esclima@gmail.com>
2022-07-18 11:44:34 -0300

    add some colors for terminal

--- index.py
+++ index.py
@@ -28,7 +28,7 @@ def createReport(entry1, entry2):
             # calculo da média
         dif = total1-total2
         average = dif/(i+1)
-        print("--"*20, "relatório", "--"*20)
+        print("\033[0;49;97m--"*20, "relatório", "--"*20)
         print("Funcionários com salários errados: ")
         for wrong in wrongSalaries:
             print(f"[ID]: {wrong['id']}","__"*5, f"[NOME]: {wrong['nome']}","__"*5, f"[SALARIO]: {wrong['salario']}", "__"*5, f"[SALARIO CORRETO]: {wrong['correto']}","__"*5, f"[DIFERENÇA]: {wrong['salario']-wrong['correto']}")
@@ -39,10 +39,14 @@ def createReport(entry1, entry2):
         print("Erro, numero de dados da empresa difere do numero de dados de entrada")
 
 # execução do programa
+blue = '\033[0;49;34m'
+green = '\033[0;49;32m'
 entrada1= []
 entrada2= []
-print("Dados de entrada")
+print(f"{blue}--"*20,"Dados de entrada","--"*20)
 readEntry(entrada1)
-print("Dados da empresa")
+print("\n\n\n")
+print(f"{green}--"*20,"Dados da empresa","--"*20)
 readEntry(entrada2)
+print("\n\n\n")
 createReport(entrada1, entrada2)
